9157fa450aea402058d548e57b99073e8b660618
gaelcorna <gael@lagencebleu.com>
2018-11-06 18:51:14 +0100

    Refactoring

--- testGeo/src/app/components/todo-detail/todo-detail.component.ts
+++ testGeo/src/app/components/todo-detail/todo-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.state';
 import * as TodoActions from '../../store/actions/todo.actions';
@@ -15,17 +15,11 @@ import { Observable } from 'rxjs';
 export class TodoDetailComponent implements OnInit {
 
   index: number;
-  private sub: any;
-  state: TodoList;
-
-  todoList$: Observable<TodoList>;
   todo: Todo;
 
   constructor( private store: Store<AppState>, private route: ActivatedRoute ) {
-    this.sub = this.route.params.subscribe( params => { this.index = +params['id']; } );
-    this.store.select('todoList').subscribe((data: TodoList) => this.state = data);
-    this.todo = this.state[this.index];
-    console.log(this.todo);
+    this.route.params.subscribe( params => { this.index = +params['id']; } );
+    this.store.select('todoList').subscribe((data: TodoList) => this.todo = data[this.index]);
   }
 
   ngOnInit() {}
